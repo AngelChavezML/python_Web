64eacd1082bba5dd43ab56e31a5e3669c824b6e1
AngelChavezML <agel.chavezmorones@mercadolibre.com.mx>
2024-05-30 12:39:58 -0600

    feat: Get with QueryParams

--- GetMethodParams/QueryParams/index.py
+++ GetMethodParams/QueryParams/index.py
@@ -10,10 +10,20 @@ class listRequestHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
 
+class queryParamRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        number = self.get_argument("num")
+        if number.isdigit():
+            r = "odd" if int(number) % 2  else "even"
+            self.write(f"The integer {number} is {r}")
+        else:
+            self.write(f"{number} is not a valid integer.")
+
 if __name__ == "__main__":
     app = tornado.web.Application([
         (r"/",basicRequestHandler),
-        (r"/animal",listRequestHandler)
+        (r"/animal",listRequestHandler),
+        (r"/iseven", queryParamRequestHandler)
     ])
 
     port = 8888
