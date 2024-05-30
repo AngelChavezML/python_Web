c528f265a67ec7076178e9f0643125e07cb94308
AngelChavezML <agel.chavezmorones@mercadolibre.com.mx>
2024-05-30 12:11:06 -0600

    feat: example of different routes

--- GetMethod/index.py
+++ GetMethod/index.py
@@ -5,9 +5,15 @@ import tornado.ioloop
 class basicRequestHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, World")
+
+class listRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
 if __name__ == "__main__":
     app = tornado.web.Application([
-        (r"/",basicRequestHandler)
+        (r"/",basicRequestHandler),
+        (r"/animal",listRequestHandler)
     ])
 
     port = 8888
